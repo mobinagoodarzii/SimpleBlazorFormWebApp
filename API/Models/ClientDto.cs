81b815d718f60cfb15855fad1e18cd076229f1a3
Mobina Goodarzi <mobina.goodarzi2000@gmail.com>
2025-02-01 09:32:05 +0330

    Add project files.

---
+++ API/Models/ClientDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class ClientDto
+    {
+        [Required(ErrorMessage ="First Name is required")]
+        public string FirstName { get; set; } = "";
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; } = "";
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Phone]
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "";
+    }
+}
